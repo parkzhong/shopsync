22c527b0299e6f1d234cf294a75fb3a020c7605e
Tan Park Zhong <tpzrocker@gmail.com>
2026-07-07 03:43:22 +0800

    Created loop menu for ShopSync

---
+++ Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int choice;
+
+    do{
+      System.out.println("===== ShopSync Lite =====");
+      System.out.println("1. Add Product");
+      System.out.println("2. View Product");
+      System.out.println("3. Exit");
+
+      System.out.print("Choose option: ");
+      choice = scanner.nextInt();
+      
+      if (choice == 1) {
+        System.out.println("Add Product selected.");
+      } else if (choice == 2) {
+        System.out.println("View Product selected.");
+      } else if (choice == 3) {
+        System.out.println("Exit selected");
+      } else {
+        System.out.println("Invalid Option.");
+      }
+
+      System.out.println();
+
+    } while (choice != 3);
+
+    scanner.close();
+  }
+}
